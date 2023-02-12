91677352f3be6409a75ac285b3485bf1218bd676
Rogério Júnior <118301838+rogerio-91@users.noreply.github.com>
2023-02-12 14:58:15 -0300

    Criação do JS e upgrade de estilização.
    Criado arquivo de JS para atender a funcionalidade do modal do menu principal.

---
+++ main.js
@@ -0,0 +1,14 @@
+// Função para o modal aparecer no Menu principal
+
+function show (){
+    document.getElementById('sobre').hidden=false;
+}
+
+show();
+
+function showFunc () {
+    document.getElementById('funcionamento').hidden=false;
+}
+
+showFunc();
+
